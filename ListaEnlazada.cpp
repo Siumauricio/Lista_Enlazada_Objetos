e492ec7c1d2ca5813a51c4488dfdc7128af21e82
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-10-24 21:44:42 -0600

    Update ListaEnlazada.cpp

--- ListaEnlazada.cpp
+++ ListaEnlazada.cpp
@@ -1,12 +1,6 @@
 #pragma once
 #include "ListaEnlazada.h"
 #include "Rectangulo.h"
-#include "TrianguloRectangulo.h"
-#include "triangulo.h"
-#include "Cuadrado.h"
-#include "Circulo.h"
-#include "Rombo.h"
-#include "Trapecio.h"
 #include "Figura2D.h"
 #include <iostream>
 using namespace std;
@@ -16,47 +10,9 @@ bool ListaEnlazada::estaVacia() {
 	return primero == nullptr;
 }
 
-void ListaEnlazada::AnadirFigura(TipoFigura tipo,int base, int altura,int Basemenor,int diagonal1,int diagonal2) {
-	Figura2D* figura;
-	Nodo* nuevo;
-	switch (tipo)
-	{
-	case rectangulo:
-		figura = new Rectangulo(base, altura);
-		 nuevo = new Nodo(figura, nullptr);
-		 AnadirAlista(nuevo);
-		break;
-	case trianguloRectangulo:
-		figura = new TrianguloRectangulo(base, altura);
-		nuevo = new Nodo(figura, nullptr);
-		AnadirAlista(nuevo);
-		break;
-	case triangulo:
-		figura = new Triangulo(base, altura);
-		nuevo = new Nodo(figura, nullptr);
-		AnadirAlista(nuevo);
-		break;
-	case cuadrado:
-		figura = new Cuadrado(base);
-		nuevo = new Nodo(figura, nullptr);
-		AnadirAlista(nuevo);
-		break;
-	case circulo:
-		figura = new Circulo(base);
-		nuevo = new Nodo(figura, nullptr);
-		AnadirAlista(nuevo);
-		break;
-	case rombo:
-		figura = new Rombo(base,altura);
-		nuevo = new Nodo(figura, nullptr);
-		AnadirAlista(nuevo);
-		break;
-	case trapecio:
-		figura = new Trapecio(base, Basemenor,altura,diagonal1,diagonal2);
-		nuevo = new Nodo(figura, nullptr);
-		AnadirAlista(nuevo);
-		break;
-	}
+void ListaEnlazada::AnadirFigura(Figura2D*figura) {
+	Nodo* nuevo = new Nodo(figura, nullptr);
+	AnadirAlista(nuevo);
 }
 
 void ListaEnlazada::AnadirAlista(Nodo* figura) {
